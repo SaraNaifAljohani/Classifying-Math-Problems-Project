864a208b6a1e977c19d8753b2b43c3fa833271f9
Sara Aljohani <96337203+SaraNaifAljohani@users.noreply.github.com>
2026-01-11 15:54:08 +0300

    Remove 'widgets' from notebook metadata
    Remove 'widgets' from notebook metadata to clean up the file.

---
+++ clean_notebook.py
@@ -0,0 +1,10 @@
+import nbformat
+
+path = "Classifying_Math_Problems.ipynb"
+nb = nbformat.read(path, as_version=4)
+
+if "widgets" in nb["metadata"]:
+    del nb["metadata"]["widgets"]
+
+nbformat.write(nb, path)
+print("metadata.widgets removed")
